7e031cf7418216655f319c294510f099521c893d
gabrielcoder <gabrielcoder247@gmail.com>
2018-11-16 12:47:17 +0300

    create class for neighborhood

--- nieghborapp/models.py
+++ nieghborapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Neighborhood(models.Model):
+    CITY_CHOICES = (
+
+        ('London', 'London'),
+        ('Moscow', 'Moscow'),
+        ('St. peterburge', 'St. peterburge'),
+        ('Nizhny Novgorod', 'Nizhny Novgorod'),
+        ('New york', 'New york'),
+        ('Abuja', 'Abuja'),
+        ('Lagos', 'Lagos'),
+        ('Perm', 'Perm'),
+        ('Kazan', 'Kazan'),
+        ('Sochi', 'Sochi'),
+    )
+
+
+
