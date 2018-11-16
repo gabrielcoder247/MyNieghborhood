cd9ca11f5f8371529dd8409374701e8b96bd6a4b
gabrielcoder <gabrielcoder247@gmail.com>
2018-11-16 14:43:49 +0300

    create image class models

--- neighborapp/models.py
+++ neighborapp/models.py
@@ -24,7 +24,8 @@ class Neighborhood(models.Model):
 
     neighborhood_name = models.CharField(max_length=30)
     neighborhood_location = models.CharField(choices=CITY_CHOICES, max_length=200 ,default=0, null=True, blank=True)
-    ocuppants_count = models.IntegerField(default=0, null=True, blank=True)
+    occupants_count = models.IntegerField(default=0, null=True, blank=True)
+    user = models.ForeignKey(User, related_name="user_neighbor", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.neighborhood_name
@@ -138,4 +139,58 @@ class Profile(models.Model):
             return self.profile_pic.url
 
     def __str__(self):
-        return self.user.username        
+        return self.user.username  
+
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name  
+
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='picture/', )
+    name = models.CharField(max_length=40)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name="images")
+    description = models.TextField()
+    location = models.ForeignKey(Location, null=True)
+    likes = models.IntegerField(default=0)
+    comments = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    @classmethod
+    def delete_image_by_id(cls, id):
+        pictures = cls.objects.filter(pk=id)
+        pictures.delete()
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        pictures = cls.objects.get(pk=id)
+        return pictures
+
+    @classmethod
+    def filter_by_location(cls, location):
+        pictures = cls.objects.filter(location=location)
+        return pictures
+
+    @classmethod
+    def search_image(cls, search_term):
+        pictures = cls.objects.filter(name__icontains=search_term)
+        return pictures
+
+    @classmethod
+    def update_image(cls, id):
+        pictures = cls.objects.filter(id=id).update(id=id)
+        return pictures           
