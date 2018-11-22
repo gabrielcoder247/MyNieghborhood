8f709213c1395fa1e8bfab32dfd27eababf27f6c
gabrielcoder <gabrielcoder247@gmail.com>
2018-11-22 12:08:48 +0300

    create .env file and install python-decouple

--- neighborapp/admin.py
+++ neighborapp/admin.py
@@ -4,8 +4,8 @@ from .models import *
 # Register your models here.
 
 
-admin.site.register(Neighborhood,NeighborhoodAdmin)
-admin.site.register(Business,BusinessAdmin)
-admin.site.register(Profile,ProfileAdmin)
-admin.site.register(Location,LocationAdmin)
-admin.site.register(Image,ImageAdmin)
+admin.site.register(Neighborhood)
+admin.site.register(Business)
+admin.site.register(Profile)
+admin.site.register(Location)
+admin.site.register(Image)
